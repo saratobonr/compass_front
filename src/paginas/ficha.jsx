86eed4a6d8a09f9c8872edb5ce717e4be868d406
Carlos Marin <carlosdaniel.marin202@gmail.com>
2022-04-29 00:10:15 -0500

    EXCEL IMPLEMENTADO Y PARTES DEL CRUD CORREGIDAS E IMPLEMENTADAS

--- src/paginas/ficha.jsx
+++ src/paginas/ficha.jsx
@@ -1,32 +1,66 @@
-import React, {useState} from 'react'
+import React, {useState,useEffect} from 'react'
 import { Link } from 'react-router-dom'
 import "./style.css";
-
+import axios from "axios";
 
 
 export function CrearFicha() {
 
+  const [select,setSelect] = useState();
     const [labs, setLabs] = useState([]);
+    const getLabs = async () => {
+      await axios
+        .get('http://localhost:5610/api/v1/laboratorios')
+        .then(res => setLabs(res.data.data))
+        .catch(err => console.log(err));
+    };
 
+    useEffect(() => {
+      getLabs();
+    }, []);
 
+    //useState para Fichas
+    const [campos, setCampos] = useState({
+      codigo_ficha:'',
+      id_laboratorio: 0,
+      fecha_emision: '',
+      version: '',
+    });
+    const postFicha = async e => {
+      let ficha = {
+        codigo_ficha:campos.codigo_ficha,
+        id_laboratorio: select.split(' ')[0],
+        fecha_emision: campos.fecha_emision,
+        version: campos.version,
+      };
+      e.preventDefault();
+      await axios.post('http://localhost:5610/api/v1/ficha', ficha);
+    };
+    const onChange = e => {
+      const value = e.target.value;
+      setCampos({
+        ...campos,
+        [e.target.name]: value,
+      });
+    };
     return (
       <div>
-        <div class="margen" id="fuente">
+        <div className="margen" id="fuente">
           
-          <div class="shadow-lg p-3 mb-5 bg-body rounded">
+          <div className="shadow-lg p-3 mb-5 bg-body rounded">
             <h1 calign="center" id="margen-titulo">
               Agregar Ficha
             </h1>
   
-            <nav class="margen">
-              <div class="row">
-                <div class="col">
+            <nav className="margen">
+              <div className="row">
+                <div className="col">
                 <br />
-              <select className="form-select-lg">
+              <select className="form-select-lg" value={select} onChange={e=>setSelect(e.target.value)}>
                 <option >Seleccione un laboratorio</option>
                 {labs.map(item => {
                   return (
-                    <option key={item.id_laboratorio}>{item.nombre_laboratorio}</option>
+                    <option key={item.id_laboratorio}>{item.id_laboratorio} {item.nombre_laboratorio}</option>
                   );
                 })}
               </select>
@@ -35,21 +69,25 @@ export function CrearFicha() {
                 
               </div>
               <br />
-              <div class="row">
-                <div class="col">
-                  <label>Versión</label>
+              <div className="row">
+                <div className="col">
+                  <label>Código</label>
                   <input
-                    class="form-control form-control-lg"
+                    className="form-control form-control-lg"
                     type="text"
-                    id="id_version"
+                    id="id_ficha"
+                    name='codigo_ficha'
+                    onChange ={onChange}
                   />
                 </div>
-                <div class="col">
-                  <label>Fecha emisión</label>
+                <div className="col">
+                  <label>Versión</label>
                   <input
-                    class="form-control form-control-lg"
+                    className="form-control form-control-lg"
                     type="text"
-                    id="fecha"
+                    id="id_version"
+                    name='version'
+                    onChange ={onChange}
                   />
                 </div>
               </div>
@@ -73,6 +111,7 @@ export function CrearFicha() {
                   id="tamaño-botones2"
                   className="btn btn-outline-success"                
                   style={{ marginleft: "10%" }}
+                  onClick={postFicha}
                 >
                   Agregar
                 </button>
