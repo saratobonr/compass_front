4d0afe9eac85ab8b7d28c8f76e7a938ea3c62e52
saratobonr <saratobon09@gmail.com>
2022-04-28 20:29:01 -0500

    -Footer y btn
    Creación footer en proto y organización botón iniciar sesión

--- src/paginas/laboratoristasHome.jsx
+++ src/paginas/laboratoristasHome.jsx
@@ -5,54 +5,56 @@ import { Link } from "react-router-dom";
 export function LaboratoristasHome() {
   return (
     <div>
-      <Link
-        style={{ textDecoration: "none", color: "#000" }}
-        to="/agregarLaboratoristas"
-      >
-        <button id="margen-btn" type="button" class="btn btn-outline-success">
-          Agregar Laboratoristas
-        </button>
-      </Link>
+      <div>
+        <Link
+          style={{ textDecoration: "none", color: "#000" }}
+          to="/agregarLaboratoristas"
+        >
+          <button id="margen-btn" type="button" class="btn btn-outline-success">
+            Agregar Laboratoristas
+          </button>
+        </Link>
 
-      <br />
-      <nav id="margen">
-        <table class="table" id="letra-tabla">
-          <thead style={{ color: "white", backgroundColor: "#aa0013" }}>
-            <tr>
-              <th scope="col">Nombre</th>
-              <th scope="col">Apellido</th>
-              <th scope="col">Usuario</th>
-              <th scope="col">Contraseña</th>
-              <th scope="col">
-                <button type="button" class="btn btn-outline-secondary">
-                  <img
-                    alt="view elements"
-                    src={require("./pencil_white.png")}
-                    width="20"
-                    height="20"
-                  />
-                </button>
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <th scope="row">Miguel</th>
-              <td>Cifuentes</td>
-              <td>mcifuentes@udemedellin.edu.co</td>
-              <td>123MiguelC</td>
-              {/* esta parte se debe poner a cada elemento agregado **/}
-              <td>
-                <div class="radio">
-                  <label>
-                    <input type="radio" id="regular" name="optradio" />
-                  </label>
-                </div>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </nav>
+        <br />
+        <nav id="margen">
+          <table class="table" id="letra-tabla">
+            <thead style={{ color: "white", backgroundColor: "#aa0013" }}>
+              <tr>
+                <th scope="col">Nombre</th>
+                <th scope="col">Apellido</th>
+                <th scope="col">Usuario</th>
+                <th scope="col">Contraseña</th>
+                <th scope="col">
+                  <button type="button" class="btn btn-outline-secondary">
+                    <img
+                      alt="view elements"
+                      src={require("./pencil_white.png")}
+                      width="20"
+                      height="20"
+                    />
+                  </button>
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">Miguel</th>
+                <td>Cifuentes</td>
+                <td>mcifuentes@udemedellin.edu.co</td>
+                <td>123MiguelC</td>
+                {/* esta parte se debe poner a cada elemento agregado **/}
+                <td>
+                  <div class="radio">
+                    <label>
+                      <input type="radio" id="regular" name="optradio" />
+                    </label>
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </nav>
+      </div>
     </div>
   );
 }
--- src/proto.jsx
+++ src/proto.jsx
@@ -6,89 +6,101 @@ export function Cuerpo() {
   return (
     <div>
       <div id="proto">
-        <div>
-          <div class="container-fluid">
-            <nav id="margen">
-              <div class="row">
-                <div class="col" id="tamaño-card">
-                  <div class="card" id="borde">
-                    <center>
-                      <Link
-                        style={{ textDecoration: "none", color: "#000" }}
-                        to="/elementosHome"
-                      >
-                        <div class="card-body">
-                          <h3 class="card-title">
-                            <br />
-                            Elementos
-                          </h3>
-                          <img
-                            alt="view elements"
-                            src={require("./imagenes/fuerte/add_elements-hardred.png")}
-                            width="210"
-                            height="210"
-                          ></img>
-                          <br />
-                        </div>
-                      </Link>
-                    </center>
-                  </div>
-                </div>
-                <div class="col" id="tamaño-card">
-                  <div class="card" id="borde">
-                    <center>
-                      <Link
-                        style={{ textDecoration: "none", color: "#000" }}
-                        to="/laboratoristasHome"
-                      >
-                        <div class="card-body">
-                          <h3 class="card-title">
-                            <br />
-                            Laboratoriastas
-                          </h3>
-                          <img
-                            alt="view elements"
-                            src={require("./imagenes/fuerte/add_user-hardred.png")}
-                            width="210"
-                            height="210"
-                          ></img>
-                          <br />
-                        </div>
-                      </Link>
-                    </center>
-                  </div>
-                </div>
-                <div class="col" id="tamaño-card">
-                  <div class="card" id="borde">
-                    <center>
-                      <Link
-                        style={{ textDecoration: "none", color: "#000" }}
-                        to="/labHome"
-                      >
-                        <div class="card-body">
-                          <h3 class="card-title">
-                            {" "}
-                            <br />
-                            Laboratorios
-                          </h3>
-                          <img
-                            alt="view elements"
-                            src={require("./imagenes/fuerte/view_lab-hardred.png")}
-                            width="200"
-                            height="210"
-                          ></img>
-                          <br />
-                        </div>
-                      </Link>
-                    </center>
-                  </div>
-                </div>
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col">
+              <div class="card" id="borde">
+                <center>
+                  <Link
+                    style={{ textDecoration: "none", color: "#000" }}
+                    to="/elementosHome"
+                  >
+                    <div class="card-body">
+                      <h3 class="card-title">
+                        <br />
+                        Elementos
+                      </h3>
+                      <img
+                        alt="view elements"
+                        src={require("./imagenes/fuerte/add_elements-hardred.png")}
+                        width="210"
+                        height="210"
+                      ></img>
+                      <br />
+                    </div>
+                  </Link>
+                </center>
               </div>
-            </nav>
+            </div>
+            <div class="col">
+              <div class="card" id="borde">
+                <center>
+                  <Link
+                    style={{ textDecoration: "none", color: "#000" }}
+                    to="/laboratoristasHome"
+                  >
+                    <div class="card-body">
+                      <h3 class="card-title">
+                        <br />
+                        Laboratoriastas
+                      </h3>
+                      <img
+                        alt="view elements"
+                        src={require("./imagenes/fuerte/add_user-hardred.png")}
+                        width="210"
+                        height="210"
+                      ></img>
+                      <br />
+                    </div>
+                  </Link>
+                </center>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card" id="borde">
+                <center>
+                  <Link
+                    style={{ textDecoration: "none", color: "#000" }}
+                    to="/labHome"
+                  >
+                    <div class="card-body">
+                      <h3 class="card-title">
+                        {" "}
+                        <br />
+                        Laboratorios
+                      </h3>
+                      <img
+                        alt="view elements"
+                        src={require("./imagenes/fuerte/view_lab-hardred.png")}
+                        width="200"
+                        height="210"
+                      ></img>
+                      <br />
+                    </div>
+                  </Link>
+                </center>
+              </div>
+            </div>
           </div>
         </div>
       </div>
       <br />
+
+      <footer class="footer">
+        <p>
+          <br />
+          Universidad de Medellín
+          <br />
+          Sección Centro de Laboratorios
+          <br />
+          Carrera 87 N° 30 – 65, Medellín – Colombia Bloque 3
+          <br />
+          Teléfono (604) 5904500 | (604) 5906999 Ext 20922 - 209923
+          <br />
+          E-mail: laboratorios@udemedellin.edu.co
+          <br />© Copyright 2021 ® Todos los Derechos Reservados.
+        </p>
+      </footer>
     </div>
   );
 }
